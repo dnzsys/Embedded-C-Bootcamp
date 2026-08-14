b6014f740b08ca6f6371519d906c3a1e11b44a11
Deniz Opak <denizopak123@gmail.com>
2026-08-14 19:28:14 +0300

    Used header file and made math functions more modular

---
+++ 02_Functions/Using_Header_File/main.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include "math.h"
+
+int main()
+{
+	printf("Sum : %d\n", math_add(1, 5));
+	printf("Sub : %d\n", math_sub(5, 1));
+	printf("Multiply : %d\n", math_add(100, 255));
+	printf("Divide : %d\n", math_add(100, 8));
+
+	return 0;
+}
---
+++ 02_Functions/Using_Header_File/math.c
@@ -0,0 +1,20 @@
+
+int math_add(int a, int b)
+{
+	return a+b;
+}
+
+int math_sub(int a, int b)
+{
+	return a+b;
+}
+
+long long int math_mult(int a, int b)
+{
+	return (long long int)a * b;
+}
+
+float math_div(int a, int b)
+{
+	return (float)a / b;
+}
