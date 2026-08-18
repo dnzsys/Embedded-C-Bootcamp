d47d1fd0138942ed38abda08824fe7bf3f9f516b
Deniz Opak <denizopak123@gmail.com>
2026-08-18 16:02:25 +0300

    Got 3 value with one scanf function

---
+++ 02_Functions/multiple_value_one_scanf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    double number1, number2, number3;
+    
+    printf("Enter 3 numbers: ");
+    scanf("%lf %lf %lf", &number1, &number2, &number3);
+    
+    double result = (number1 + number2 + number3) / 3;
+    printf("\nAverage of this numbers is : %0.1lf\n", result);
+    
+    printf("Press enter key for exit the program\n");
+    while(getchar() != '\n'); // used that because of the buffer
+    getchar();
+    
+    return 0;
+}
