63b45c7a70b0158c1c41ccf4bb8f0932dec30c6d
Deniz Opak <denizopak123@gmail.com>
2026-08-18 12:22:18 +0300

    Started using float and double data types

---
+++ 01_Variables/float_double.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	float number = 52.1231414512;
+	printf("Number is : %f\n", number); // prints 52.123142
+
+	double number2 = 52.1231414512;
+	printf("Number is : %0.14lf", number2); // prints 52.1231414512;
+
+	return 0;
+}
