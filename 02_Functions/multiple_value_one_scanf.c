3c0b746fce04db9c7f3fc4c8ea74fe0e7fd9fe80
Deniz Opak <denizopak123@gmail.com>
2026-08-20 18:15:20 +0300

    Fixed buffer problem

--- 02_Functions/multiple_value_one_scanf.c
+++ 02_Functions/multiple_value_one_scanf.c
@@ -5,6 +5,7 @@ int main()
     double number1, number2, number3;
     
     printf("Enter 3 numbers: ");
+    fflush(stdout);
     scanf("%lf %lf %lf", &number1, &number2, &number3);
     
     double result = (number1 + number2 + number3) / 3;
