3048f9a244c4faee961ffd3d2b54bad48d3e53bd
Deniz Opak <denizopak123@gmail.com>
2026-08-21 15:25:37 +0300

    Made vote checker with else

---
+++ 02_Functions/cast_vote_checker_else_version.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int age;
+    
+    printf("Please enter your age : ");
+    fflush(stdout);
+    scanf("%d", &age);
+    
+    if(age>=18)
+    {
+        printf("\nYou can vote!");
+    }
+    else
+    {
+        printf("\nYou can't vote!");
+    }
+
+    return 0;
+}
