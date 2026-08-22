021d75a7cddd9c69ebdd069ee4fed9230f341c8a
Deniz Opak <denizopak123@gmail.com>
2026-08-22 22:55:23 +0300

    feat: made basic toggler with bitwise operators

---
+++ 04_Bitwise_Operations/toggler.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void waitForEnd();
+
+int main()
+{ 
+    int input, output;
+    
+    printf("Enter a number : ");
+    fflush(stdout);
+    scanf("%d", &input);
+    
+    output = input ^ 0x01;
+    
+    printf("[input] [output] : %x %x\n", input, output);
+    
+    waitForEnd();
+
+    return 0;
+}
+
+void waitForEnd()
+{
+    printf("\nPress enter to exit the application");
+    while(getchar() != '\n');
+    getchar();
+}
